fc436ccca04523d1ef6fa79ab556102ab01b17b0
Mayerstrk <45215789+Mayerstrk@users.noreply.github.com>
2023-03-16 20:08:39 +0200

    refactor(routes): move helpers into helpers.ts file

---
+++ helpers.ts
@@ -0,0 +1,44 @@
+import { promises as fsPromises } from 'node:fs';
+import path from 'node:path';
+import { type Request, type Response } from 'express';
+
+import usersData from './data/users.json';
+import cardsData from './data/cards.json';
+
+const USERS_PATH = path.join(__dirname, 'data/users.json');
+
+type Helper = (request: Request, response: Response) => void;
+
+type UserData = {
+	name: string;
+	about: string;
+	avatar: string;
+	_id: string;
+};
+
+const getUsers: Helper = (request, response) => {
+	response.send(usersData);
+};
+
+const getUser: Helper = (request: Request, response: Response) => {
+	fsPromises.readFile(USERS_PATH, { encoding: 'utf8' }).then(users => {
+		const parsedUserData = JSON.parse(users);
+		const user = parsedUserData.find((user: UserData) => user._id === request.params.id);
+		if (user) {
+			response.send(user);
+		} else {
+			response.status(400);
+			response.send({ message: 'User not found' });
+		}
+	})
+		.catch(() => {
+			response.status(500);
+			response.send({ message: 'Unexpected error' });
+		});
+};
+
+const getCards: Helper = (request, response) => {
+	response.send(cardsData);
+};
+
+export { getUsers, getUser, getCards };
